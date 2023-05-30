cbe374050bac055d8ee3edd02de035d5ddbbe262
Lepre-CHAU-n <71118130+Lepre-CHAU-n@users.noreply.github.com>
2023-05-30 10:49:04 -0700

    edit codez

--- src/main/resources/static/js/calculate.js
+++ src/main/resources/static/js/calculate.js
@@ -2,7 +2,7 @@ var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22
     49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
 let histogramContainer = document.querySelector('.histogram');
 let enterGradeInput = document.getElementById('enterGrade');
-
+//var grades=[];
 
 
 

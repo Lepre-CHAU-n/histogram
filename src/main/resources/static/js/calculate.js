e1407b7abc536b074c315d8a9f8a6f40c3d9b06d
Lepre-CHAU-n <71118130+Lepre-CHAU-n@users.noreply.github.com>
2023-05-30 10:28:41 -0700

    edit codes

--- src/main/resources/static/js/calculate.js
+++ src/main/resources/static/js/calculate.js
@@ -1,18 +1,18 @@
 var grades = [65.95, 56.98, 78.62, 96.1, 90.3, 72.24, 92.34, 60.00, 81.43, 86.22, 88.33, 9.03,
     49.93, 52.34, 53.11, 50.10, 88.88, 55.32, 55.69, 61.68, 70.44, 70.54, 90.0, 71.11, 80.01];
 // var grades= []; 
-var histogramContainer = document.querySelector('.histogram');
-var enterGradeInput = document.getElementById('enterGrade');
+let histogramContainer = document.querySelector('.histogram');
+let enterGradeInput = document.getElementById('enterGrade');
 
 
 
 
-var errorMessage = document.getElementById('errorMessage');
-var errorMessage1 = document.getElementById('errorMessage1');
+let errorMessage = document.getElementById('errorMessage');
+let errorMessage1 = document.getElementById('errorMessage1');
 
 function updateHistogram() {
-   var newLowerBounds = getLowerBounds();
-   var variables = [
+   let newLowerBounds = getLowerBounds();
+   let variables = [
        { id: 'aPlus', variable: 'a' },
        { id: 'aNeutral', variable: 'a1' },
        { id: 'aMinus', variable: 'a2' },
@@ -25,16 +25,16 @@ function updateHistogram() {
        { id: 'dNeutral', variable: 'd' }
    ];
 
-   for (var i = 0; i < variables.length; i++) {
-       var element = document.getElementById(variables[i].id);
-       var value = parseFloat(element.value);
-       if (isNaN(value)) {
+   for (let i = 0; i < variables.length; i++) {
+       let element = document.getElementById(variables[i].id);
+       let value = parseFloat(element.value);
+       if (Number.isNaN(value)) {
            errorMessage.textContent = 'Invalid lower bounds. Please make sure the values are in ascending order and/or enter a number for each grade.';
            element.value = ''; // Clear the input text
            return;
        } else {
-           var inputs = element.value;
-           var sanitizedInputs = inputs.replace(/[^0-9.]/g, ''); // Allow decimal numbers
+           let inputs = element.value;
+           let sanitizedInputs = inputs.replace(/[^0-9.]/g, ''); // Allow decimal numbers
            element.value = sanitizedInputs;
            errorMessage.textContent = ''; // Clear the error message
        }
@@ -42,14 +42,14 @@ function updateHistogram() {
 
    if (validateLowerBounds(newLowerBounds)) {
        // Validate each lower bound for letters
-       for (var j = 0; j < newLowerBounds.length; j++) {
-           if (isNaN(newLowerBounds[j])) {
+       for (let j = 0; j < newLowerBounds.length; j++) {
+           if (Number.isNaN(newLowerBounds[j])) {
                errorMessage.textContent = 'Invalid lower bounds. Please make sure the values are in ascending order and/or enter a number for each grade.';
                return;
            }
        }
        errorMessage.textContent = ''; // Clear the error message
-       var histogram = calculateHistogram(grades, newLowerBounds);
+       let histogram = calculateHistogram(grades, newLowerBounds);
        displayHistogram(histogram);
    } else {
        errorMessage.textContent = 'Invalid lower bounds. Please make sure the values are in ascending order and/or enter a number for each grade.';
@@ -61,7 +61,7 @@ function updateHistogram() {
 
 
 function getLowerBounds() {
-  var lowerBounds = [];
+  let lowerBounds = [];
   lowerBounds.push(parseFloat(document.getElementById('Max').value));
   lowerBounds.push(parseFloat(document.getElementById('aPlus').value));
   lowerBounds.push(parseFloat(document.getElementById('aNeutral').value));
@@ -78,7 +78,7 @@ function getLowerBounds() {
 }
 
 function validateLowerBounds(lowerBounds) {
-  for (var i = 1; i < lowerBounds.length; i++) {
+  for (let i = 1; i < lowerBounds.length; i++) {
       if (lowerBounds[i] > lowerBounds[i - 1]) {
           return false;
       }
@@ -87,16 +87,16 @@ function validateLowerBounds(lowerBounds) {
 }
 
 function calculateHistogram(grades, lowerBounds) {
-   var histogram = [];
-   for (var i = 0; i < lowerBounds.length; i++) {
-       var count = 0;
-       var a = 0;
-       for (var j = 0; j < grades.length; j++) {
+   let histogram = [];
+   for (let i = 0; i < lowerBounds.length; i++) {
+       let count = 0;
+       let a = 0;
+       for (let j = 0; j < grades.length; j++) {
         if (grades[j] > lowerBounds[i]) {
             a++;
         } else if(grades[j] === lowerBounds[0]){
             count++;
-        }  else if (grades[j] >= lowerBounds[i + 1] && grades[j] < lowerBounds[i]) {
+        }else if (grades[j] >= lowerBounds[i + 1] && grades[j] < lowerBounds[i]) {
             count++;
         } 
        }
@@ -108,36 +108,36 @@ function calculateHistogram(grades, lowerBounds) {
 
 function displayHistogram(histogram) {
    histogramContainer.innerHTML = '';
-   var gradeLabels = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D', 'F'];
-   var histogramTitle = document.createElement('h3');
+   let gradeLabels = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D', 'F'];
+   let histogramTitle = document.createElement('h3');
    histogramTitle.textContent = 'Histogram';
    histogramContainer.appendChild(histogramTitle);
 
-   var maxCount = Math.max(...histogram); // Get the maximum count for scaling the bar width
-   var histogramBarHeight = '20px'; // Height of the histogram bars
-   var maxBarWidth = 300; // Maximum width of the histogram bar container (adjust as needed)
+   let maxCount = Math.max(...histogram); // Get the maximum count for scaling the bar width
+   let histogramBarHeight = '20px'; // Height of the histogram bars
+   let maxBarWidth = 300; // Maximum width of the histogram bar container (adjust as needed)
 
-   for (var i = 0; i < histogram.length - 1; i++) { // Adjust loop condition to exclude the extra bottom bar
-       var gradeDiv = document.createElement('div');
+   for (let i = 0; i < histogram.length - 1; i++) { // Adjust loop condition to exclude the extra bottom bar
+       let gradeDiv = document.createElement('div');
        gradeDiv.style.display = 'flex'; // Set display to flex for aligning grade label and histogram bar
        gradeDiv.style.alignItems = 'center'; // Align items vertically in the flex container
        gradeDiv.style.width = maxBarWidth + 'px'; // Set fixed width for the gradeDiv container
 
-       var gradeLabel = document.createElement('p');
+       let gradeLabel = document.createElement('p');
        gradeLabel.textContent = gradeLabels[i];
        gradeLabel.style.width = '40px'; // Set fixed width for the grade label
 
-       var histogramBarContainer = document.createElement('div');
+       let histogramBarContainer = document.createElement('div');
        histogramBarContainer.style.width = maxBarWidth - 50 + 'px'; // Set fixed width for the histogram bar container
        histogramBarContainer.style.backgroundColor = 'lightgray';
        histogramBarContainer.style.height = histogramBarHeight;
 
-       var histogramBar = document.createElement('div');
+       let histogramBar = document.createElement('div');
        histogramBar.style.width = (histogram[i] / maxCount) * 100 + '%'; // Scale the bar width based on the maximum count
        histogramBar.style.backgroundColor = 'gray';
        histogramBar.style.height = histogramBarHeight;
 
-       var gradeCount = document.createElement('span');
+       let gradeCount = document.createElement('span');
        gradeCount.textContent = histogram[i]; // Display the count for the current grade
 
        histogramBarContainer.appendChild(histogramBar);
@@ -149,7 +149,7 @@ function displayHistogram(histogram) {
 }
 
 enterGradeInput.addEventListener('keydown', function(event) {
-    var invalidChars = ['e', 'E', '+', '-'];
+    let invalidChars = ['e', 'E', '+', '-'];
     if (invalidChars.includes(event.key)){
         event.preventDefault();
     }
@@ -157,10 +157,10 @@ enterGradeInput.addEventListener('keydown', function(event) {
 
 enterGradeInput.addEventListener('keyup', function(event) {
    if (event.key === 'Enter' ) {
-       var grade = parseFloat(enterGradeInput.value);
+       let grade = parseFloat(enterGradeInput.value);
        enterGradeInput.value = '';
        errorMessage1.textContent = ''; // Clear the error message
-       if (isNaN(grade) || grade < 0 || grade > 100) {
+       if (Number.isNaN(grade) || grade < 0 || grade > 100) {
            errorMessage1.textContent = 'Invalid grade. Please enter a number between 0 and 100.';
            return;
        }
